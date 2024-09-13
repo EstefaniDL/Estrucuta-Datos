156dff3fbb523b54eaaf2d32aeb0397ecd965e77
Fany_Lira <321339968@pcpuma.acatlan.unam.mx>
2024-09-13 13:43:33 -0600

    Add files via upload

---
+++ «Tareas/Diagramas/Programas/dynamic_memory.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int *iptr;
+	
+	iptr = (int *)malloc(sizeof(int));
+	
+	if (iptr == NULL){
+		return -1;
+	}
+	
+	*iptr = 5;
+	
+	printf("%d , %p \n",*iptr,iptr);
+	free(iptr);
+	
+	return 0;
+}
