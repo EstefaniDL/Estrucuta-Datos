b22f0ff2965b08ea42582b4f1a63ed258f96306e
Fany_Lira <321339968@pcpuma.acatlan.unam.mx>
2024-10-13 17:21:27 -0600

    Update cdlist.h

--- Code/include/cdlist.h
+++ Code/include/cdlist.h
@@ -1,1 +1,17 @@
+#ifndef LIST_H
+#define LIST_H
 
+// Definicion del nodo
+typedef struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+} Node;
+
+// Funciones para manejar la lista
+Node* create_node(int data);
+void insert_end(Node** head, int data);
+void display_list(Node* head);
+void free_list(Node* head);
+
+#endif
